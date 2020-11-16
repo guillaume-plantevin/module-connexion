ededdb3e0d3eda03befc8e00c9e86d9cdd61f12c
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-16 19:33:51 +0100

    Commit.01: debut php et htmp

---
+++ inscription.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="styles/style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>inscription</title>
+    </head>
+    <body>
+        <form action="connexion.php" method="POST">
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="login"><br />
+
+            <label for="fName">Prenom:</label>
+            <input type="text" name="prenom" id="fName"><br />
+
+            <label for="lName">Nom:</label>
+            <input type="text" name="nom" id="lName"><br />
+
+            <label for="password">Mot de passe:</label>
+            <input type="password" name="password" id="password"><br />
+
+            <label for="passwordConfirm">Confirmation du mot de passe:</label>
+            <input type="password" name="passwordConfirm"><br />
+        </form>
+    </body>
+</html>
+
+<!-- 
+    Le formulaire doit contenir l’ensemble des champs présents dans la table
+    “utilisateurs” (sauf “id”) + une confirmation de mot de passe. Dès qu’un
+    utilisateur remplit ce formulaire, les données sont insérées dans la base de
+    données et l’utilisateur est redirigé vers la page de connexion
+ -->
---
+++ profil.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<!-- 
+    Cette page possède un formulaire permettant à l’utilisateur de modifier ses
+    informations. Ce formulaire est par défaut pré-rempli avec les informations
+    qui sont actuellement stockées en base de données.
+ -->
